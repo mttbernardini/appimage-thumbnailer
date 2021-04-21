6781f1b3a25b0744c396364ad50da5e63d7acbd9
Matteo Bernardini <mat_teobe@yahoo.it>
2021-04-21 10:18:43 +0200

    ci: build script tweaks

--- ci/build.sh
+++ ci/build.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-cd "$(dirname "$0")"
+cd "$(dirname "$(realpath "$0")")"
 docker build -t appimage-thumbnailer -f Dockerfile .
 docker run -i --rm --user "$(id -u)" \
 	-v "$(realpath ..):/ws" \
 	appimage-thumbnailer \
-	sh -xc "cmake -B build . && cmake --build build" \
+	sh -exc "cmake -B build . && cmake --build build"
