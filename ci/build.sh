667143f77c39fac8502bc4200459046710a31433
Matteo Bernardini <mat_teobe@yahoo.it>
2021-04-22 16:06:53 +0200

    ci: allow custom build commands

--- ci/build.sh
+++ ci/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -ex
 
+if [ -n "$1" ]; then
+	BUILD_CMD="$1"
+else
+	BUILD_CMD="cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -B build . && cmake --build build"
+fi
+
 cd "$(dirname "$(realpath "$0")")"
 docker build -t appimage-thumbnailer -f Dockerfile .
 docker run -i --rm --user "$(id -u)" \
 	-v "$(realpath ..):/ws" \
 	appimage-thumbnailer \
-	sh -exc "cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -B build . && cmake --build build"
+	sh -exc "$BUILD_CMD"
