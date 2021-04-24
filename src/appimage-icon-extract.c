d465e1d1c010b29b2cf1eb662ac8ca336c82b71a
Matteo Bernardini <mat_teobe@yahoo.it>
2021-04-24 18:46:12 +0200

    icon extract: changed API (return custom type instead of FILE*)

--- src/appimage-icon-extract.c
+++ src/appimage-icon-extract.c
@@ -2,25 +2,23 @@
  * Copyright (c) 2021 Matteo Bernardini
  */
 
-#include <stdio.h>
-#include <stdbool.h>
 #include <appimage/appimage.h>
-
 #include "appimage-icon-extract.h"
 
-FILE* appimage_thumbnailer_icon_extract(char* path_to_appimage, int preferred_size)
+icon_t appimage_thumbnailer_icon_extract(char* path_to_appimage, int preferred_size)
 {
 	/**
 	 * Extracts AppImage icon. If an icon of `preferred_size` is found it will be returned, otherwise fallback to `.DirIcon`.
 	 * If not found (non-compliant AppImage), `NULL` is returned
 	 */
 
-	char* buffer;
-	size_t buf_size;
-	bool found = appimage_read_file_into_buffer_following_symlinks(path_to_appimage, ".DirIcon", &buffer, &buf_size);
+	icon_t icon;
+	bool found = appimage_read_file_into_buffer_following_symlinks(path_to_appimage, ".DirIcon", &icon.data, &icon.data_size);
 
 	if (!found)
-		return NULL;
+	{
+		icon.data = NULL;
+	}
 
-	return fmemopen(buffer, buf_size, "r");
+	return icon;
 }
