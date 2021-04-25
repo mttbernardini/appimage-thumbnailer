fee40db9b71620a5b0378033017ab834863ece08
Matteo Bernardini <mat_teobe@yahoo.it>
2021-04-25 12:11:06 +0200

    removed appimage mime: already provided by shared-mime-info (at least in debian buster)

--- cmake/cpack_deb.cmake
+++ cmake/cpack_deb.cmake
@@ -2,13 +2,11 @@ set(CPACK_DEB_COMPONENT_INSTALL ON)
 set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
 set(CPACK_DEBIAN_COMPRESSION_TYPE xz)
 set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
-set(CPACK_DEBIAN_PACKAGE_PROVIDES "appimage-mimetype") # in the future it might be better to have a unified mime provider as a separate package
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "shared-mime-info") # provides appimage mime type
 set(CPACK_DEBIAN_PACKAGE_SUGGESTS "appimagelauncher")
 
 # gnome thumbnailer
 set(CPACK_DEBIAN_APPIMAGE-THUMBNAILER-GNOME_PACKAGE_NAME "gnome-appimage-thumbnailer")
-#set(CPACK_DEBIAN_APPIMAGE-THUMBNAILER-GNOME_PACKAGE_DEPENDS "libgdk-pixbuf-2.0-0")
 
 # kde thumbnailer
 set(CPACK_DEBIAN_APPIMAGE-THUMBNAILER-KDE_PACKAGE_NAME "kde-appimage-thumbnailer")
-#set(CPACK_DEBIAN_APPIMAGE-THUMBNAILER-KDE_PACKAGE_DEPENDS "libkf5kiocore5 libqt5core5a")
