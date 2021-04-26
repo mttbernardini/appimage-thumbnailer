88af97493f353f2aa093a5f0d05feeb340d730e2
Matteo Bernardini <mat_teobe@yahoo.it>
2021-04-26 15:34:35 +0200

    gnome: rescale icon to 52x52

--- src/gnome-appimage-thumbnailer.c
+++ src/gnome-appimage-thumbnailer.c
@@ -3,7 +3,48 @@
  */
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <gio/gio.h>
+#include <gdk-pixbuf/gdk-pixbuf.h>
 #include "appimage-icon-extract.h"
+
+static GdkPixbuf* rescale_icon(ait_icon_t* icon, unsigned target_size)
+{
+	GInputStream* icon_stream = NULL;
+	GdkPixbuf* icon_pixbuf = NULL;
+	GdkPixbuf* rescaled_pixbuf = NULL;
+	GError* icon_error = NULL;
+
+	icon_stream = g_memory_input_stream_new_from_data(icon->data, icon->data_size, &appimage_thumbnailer_destroy_icon);
+	if (!icon_stream)
+	{
+		fputs("AIT - cannot allocate GInputStream\n", stderr);
+		goto rescale_icon_epilogue;
+	}
+
+	icon_pixbuf = gdk_pixbuf_new_from_stream(icon_stream, NULL, &icon_error);
+	if (!icon_pixbuf)
+	{
+		fprintf(stderr, "AIT - error creating GdkPixbuf: %s\n", icon_error->message);
+		goto rescale_icon_epilogue;
+	}
+
+	rescaled_pixbuf = gdk_pixbuf_scale_simple(icon_pixbuf, target_size, target_size, GDK_INTERP_BILINEAR);
+
+	if (!rescaled_pixbuf)
+	{
+		fputs("AIT - cannot allocate resized GdkPixbuf\n", stderr);
+		goto rescale_icon_epilogue;
+	}
+
+rescale_icon_epilogue:
+	g_clear_object(&icon_stream);
+	g_clear_object(&icon_pixbuf);
+	g_clear_error(&icon_error);
+
+	return rescaled_pixbuf;
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -27,16 +68,14 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	FILE* out = fopen(output_path, "w");
-	if (!out)
+	GdkPixbuf* rescaled_icon = rescale_icon(icon, size);
+	if (!rescaled_icon)
 	{
 		return 1;
 	}
 
-	// TODO: resize icon and write to png
-	fwrite(icon->data, 1, icon->data_size, out);
-	appimage_thumbnailer_destroy_icon(icon);
-	fclose(out);
+	bool saved = gdk_pixbuf_save(rescaled_icon, output_path, "png", NULL, NULL);
+	g_clear_object(&rescaled_icon);
 
-	return 0;
+	return saved ? 0 : 1;
 }
